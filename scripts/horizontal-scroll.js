827dd6cd3fdab39ae2b705aecd78367d57adf985
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-08-21 14:49:46 +0530

    our company section and why rms - responsive complete || horizontal scroll tweak for mobile

--- scripts/horizontal-scroll.js
+++ scripts/horizontal-scroll.js
@@ -3,10 +3,12 @@ const ourCompanyText = document.querySelector(".our-company-text");
 gsap.fromTo(
   ourCompanyText,
   {
-    xPercent: 10, // Start right if 100 used it expands horizontal scroll big
+    xPercent: 10,
+    margin: "20rem 50rem", // Start right if 100 used it expands horizontal scroll big
   },
   {
-    xPercent: -100, // End to the left (off the viewport)
+    xPercent: -100,
+    margin: "20rem 0rem", // End to the left (off the viewport)
     scrollTrigger: {
       trigger: ourCompanyText, // which elemnt to target
       start: "top bottom", // scroll start type
